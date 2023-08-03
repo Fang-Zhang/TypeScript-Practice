38fd72a12aa6a1fd97482227e49645aa498dcbc4
Walter Zhang <walter.zhangfang@gmail.com>
2023-08-03 19:40:20 +1200

    update

--- text.ts
+++ text.ts
@@ -1,24 +1,32 @@
-type User = {
-    name: string;
-    email: string;
-    isActive: boolean;
+// type User = {
+//     name: string;
+//     email: string;
+//     isActive: boolean;
+// }
+
+// function createUser(user: User): User{
+//     return {name: "", email: "", isActive: false}
+// }
+
+// createUser({name: "", email: "", isActive: false})
+
+// type Point = {
+//     x: number;
+//     y: number;
+// }
+
+// function printPoint(point: Point){
+//     console.log(point.x, point.y)
+// }
+
+// let scrore: number | string = 100;
+
+function provideId(id: string){
+    if (typeof id === "string"){
+        return id.toLowerCase();
+    } 
+    return id;
+    
 }
-
-function createUser(user: User): User{
-    return {name: "", email: "", isActive: false}
-}
-
-createUser({name: "", email: "", isActive: false})
-
-type Point = {
-    x: number;
-    y: number;
-}
-
-function printPoint(point: Point){
-    console.log(point.x, point.y)
-}
-
-
 
 export {}
